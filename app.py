655d6b68800290aac74b399e909cc0ecbe2504de
salman91ahmed <salman.ahmed.a35@gmail.com>
2024-12-16 21:27:26 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+#Data types with variables
+print("Hello World")
+
+crname:str = "Salman"
+print(crname)
+
+Marks:int=80
+print(Marks)
+Allowed:bool=True
+print(Allowed)
+Marks:int="Salman"
+print(Marks)
+print("Salman")
+
